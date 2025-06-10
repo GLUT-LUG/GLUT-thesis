73846964b73b9ee0bd933cfebcf33650f16d9ad7
Ntkskwk <natsukawa247@outlook.com>
2025-06-10 09:01:43 +0800

    feat: Cleaning up intermediate documents

--- thesis/template/clean.sh
+++ thesis/template/clean.sh
@@ -1,2 +1,2 @@
 find . -name '*.aux' -print0 | xargs -0 rm -rf
-rm -rf *.lof *.log *.lot *.out *.toc *.bbl *.blg *.thm *.nav *.xml *.snm *.bcf
+rm -rf *.lof *.log *.lot *.out *.toc *.bbl *.blg *.thm *.nav *.xml *.snm *.bcf *.fls *.fdb_latexmk
